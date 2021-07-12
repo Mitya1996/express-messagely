2d5acbaa017c7adc5c1e92a6a308952add59398e
mits <dmitrybaycharov@gmail.com>
2021-07-12 17:12:39 -0500

    part 1

--- models/user.js
+++ models/user.js
@@ -1,29 +1,42 @@
+const db = require("../db");
+
 /** User class for message.ly */
-
-
 
 /** User of the site. */
 
 class User {
-
   /** register new user -- returns
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({ username, password, first_name, last_name, phone }) {
+    const result = await db.query(
+      `INSERT INTO messages (
+        username,
+        password,
+        first_name,
+        last_name,
+        phone,)
+          VALUES ($1, $2, $3, $4, $5)
+          RETURNING username, password, first_name, last_name, phone`,
+      [username, password, first_name, last_name, phone]
+    );
+
+    return result.rows[0];
+  }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+  static async authenticate(username, password) {}
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+  static async updateLoginTimestamp(username) {}
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
 
-  static async all() { }
+  static async all() {}
 
   /** Get: get user by username
    *
@@ -34,7 +47,7 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+  static async get(username) {}
 
   /** Return messages from this user.
    *
@@ -44,7 +57,7 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) { }
+  static async messagesFrom(username) {}
 
   /** Return messages to this user.
    *
@@ -54,8 +67,7 @@ class User {
    *   {id, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) { }
+  static async messagesTo(username) {}
 }
-
 
 module.exports = User;
