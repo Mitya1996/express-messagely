31e0660d792257f951ec0636ad652e4668eeec26
mits <dmitrybaycharov@gmail.com>
2021-07-13 15:30:04 -0500

    models/user.js completed

--- models/user.js
+++ models/user.js
@@ -51,7 +51,6 @@ class User {
          RETURNING username, last_login_at`,
       [username]
     );
-    debugger;
     if (!result.rows[0]) {
       throw new ExpressError(`No such user: ${username}`, 404);
     }
@@ -62,7 +61,12 @@ class User {
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
 
-  static async all() {}
+  static async all() {
+    const result = await db.query(
+      `SELECT username, first_name, last_name, phone FROM users`
+    );
+    return result.rows;
+  }
 
   /** Get: get user by username
    *
@@ -73,7 +77,17 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) {}
+  static async get(username) {
+    const result = await db.query(
+      `SELECT username, first_name, last_name, phone, join_at, last_login_at FROM users WHERE username=$1`,
+      [username]
+    );
+    if (!result.rows[0]) {
+      throw new ExpressError(`No such user: ${username}`, 404);
+    }
+
+    return result.rows[0];
+  }
 
   /** Return messages from this user.
    *
@@ -83,7 +97,25 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) {}
+  static async messagesFrom(username) {
+    const result = await db.query(
+      `SELECT m.id, m.to_username, m.body, m.sent_at, m.read_at, u.first_name, u.last_name, u.phone FROM messages AS m JOIN users AS u ON m.to_username = u.username WHERE from_username=$1`,
+      [username]
+    );
+
+    return result.rows.map((m) => ({
+      id: m.id,
+      to_user: {
+        username: m.to_username,
+        first_name: m.first_name,
+        last_name: m.last_name,
+        phone: m.phone,
+      },
+      body: m.body,
+      sent_at: m.sent_at,
+      read_at: m.read_at,
+    }));
+  }
 
   /** Return messages to this user.
    *
@@ -93,7 +125,25 @@ class User {
    *   {id, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) {}
+  static async messagesTo(username) {
+    const result = await db.query(
+      `SELECT m.id, m.from_username, m.body, m.sent_at, m.read_at, u.first_name, u.last_name, u.phone FROM messages AS m JOIN users AS u ON m.from_username = u.username WHERE to_username=$1`,
+      [username]
+    );
+
+    return result.rows.map((m) => ({
+      id: m.id,
+      from_user: {
+        username: m.from_username,
+        first_name: m.first_name,
+        last_name: m.last_name,
+        phone: m.phone,
+      },
+      body: m.body,
+      sent_at: m.sent_at,
+      read_at: m.read_at,
+    }));
+  }
 }
 
 module.exports = User;
