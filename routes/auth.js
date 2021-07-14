887c97c1acf335e15cd73b04e819edf8ef10f249
mits <dmitrybaycharov@gmail.com>
2021-07-14 11:50:29 -0500

    login route created

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,30 @@
+//express router
+const Router = require("express").Router;
+const router = new Router();
+
+const { SECRET_KEY } = require("../config");
+const ExpressError = require("../expressError");
+
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
-
+router.post("/login", async function (req, res, next) {
+  try {
+    const { username, password } = req.body;
+    if (await User.authenticate(username, password)) {
+      let token = jwt.sign({ username }, SECRET_KEY);
+      User.updateLoginTimestamp(username);
+      return res.json(token);
+    } else throw new ExpressError("Invalid username/password", 400);
+  } catch (e) {
+    return next(e);
+  }
+});
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
